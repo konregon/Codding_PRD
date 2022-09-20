2365557074d614c0ad7b21caaf42329448cb1ea3
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-09-20 18:55:56 +0530

    js logic

--- 4th week of Sep-22/20-09-2022/DGClock.js
+++ 4th week of Sep-22/20-09-2022/DGClock.js
@@ -1,6 +1,39 @@
-let a = ['a', null, 'c'];
-let b = ['a', undefined, 'c'];
-console.log(a);
+// Create 4 constant which we want to update 
+const hourEL = document.getElementById("Hours");
+const minuteEl = document.getElementById("Minutes");
+const secondEl = document.getElementById("Seconds");
+const ampmEl = document.getElementById("ampm");
 
-console.log(b);
+// make function to update clock
+function updateClock(){
+    // create 4 variable because we want them change their value by time
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
 
+    // if condition for changing AM and PM
+    if(h > 12){
+        h = h - 12
+        ampm = "PM"
+    }
+
+    // condition Boolean for displaying 0 before any single digit. ie. 04 instead of 4
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    // code for to display text in browser
+    hourEL.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+
+    // timer condition to update time every second
+    setTimeout(() => {
+        updateClock();
+    }, 1000);
+}
+
+// current time when this script first time load
+updateClock();
